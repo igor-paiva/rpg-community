d1e30072ce81989af50dad0ef19e535f43476c54
Rhuan Carlos <rhuancarlos.queiroz@gmail.com>
2019-07-06 11:11:58 -0300

    Configuring sing_in and sign_up views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  respond_to :html, :xml, :json
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :sign_up,
-      keys: %i[name nick_name twitter instagram description phone]
+      keys: %i[name email nick_name twitter instagram description phone]
     )
   end
 end
