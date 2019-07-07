b4e39b6964d5b34cdc0fbc84fb7ca88afa4e19ea
Igor Batista Paiva <igorbatistapaiva@outlook.com>
2019-07-07 12:31:23 -0300

    Refactoring masters availables method test

--- spec/models/master_spec.rb
+++ spec/models/master_spec.rb
@@ -5,21 +5,44 @@ RSpec.describe Master, :type => :model do
     let!(:no_master_user) { create(:user) }
     let!(:no_user_master) { create(:master) }
 
+    let(:not_available_master_user) { create(:user) }
+    let(:not_available_master) { create(:master, available: false) }
+
+    let!(:not_available_role) do
+      create(
+        :role,
+        user: not_available_master_user,
+        master: not_available_master
+      )
+    end
+
     subject { described_class.availables }
 
     context 'when there is no masters' do
-      it 'is empty' do
+      it 'return nothing' do
         is_expected.to be_empty
       end
     end
 
     context 'when there is only one master' do
-      let(:user) { create(:user) }
-      let(:master) { create(:master) }
-      let!(:role) { create(:role, user: user, master: master) }
+      context 'and it is not available' do
+        let(:user) { create(:user) }
+        let(:master) { create(:master, available: false) }
+        let!(:role) { create(:role, user: user, master: master) }
 
-      it 'return the master user' do
-        is_expected.to match_array [user]
+        it 'return nothing' do
+          is_expected.to be_empty
+        end
+      end
+
+      context 'and it is available' do
+        let(:user) { create(:user) }
+        let(:master) { create(:master) }
+        let!(:role) { create(:role, user: user, master: master) }
+
+        it 'return the master user' do
+          is_expected.to match_array [user]
+        end
       end
     end
 
