c4c2eb06feee9cd2876a9bb3f7954898e3376208
Rhuan Carlos <rhuancarlos.queiroz@gmail.com>
2019-07-08 12:47:23 -0300

    Fixed the routes properly Added blank lines to index and show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :tables
-
   resources :players, only: [:index]
 
   resources :masters, only: [:index]
 
   resources :tables do
     get '/users/:admin_id/',
-        action: :user_tables,
-        controller: 'tables',
+        to: 'tables#user_tables',
         on: :collection
   end
 end
