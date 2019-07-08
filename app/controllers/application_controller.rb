ebbffc679e5b2fc8087fb4c67b87247619346b4b
Rhuan Carlos <rhuancarlos.queiroz@gmail.com>
2019-07-08 01:05:43 -0300

    Adding not found default page error to avoid bad url params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found
+    head :not_found
+  end
+
   protected
 
   def configure_permitted_parameters
