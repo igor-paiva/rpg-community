f146995e8bd932b6059b081b052fcc3fc4e6d79e
Igor Batista Paiva <igorbatistapaiva@outlook.com>
2019-07-09 02:58:04 -0300

    Add validation to user name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   has_many :tables, foreign_key: :admin_id, dependent: :destroy
   has_many :solicitations, dependent: :destroy
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
