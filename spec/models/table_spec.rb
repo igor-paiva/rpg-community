75f0dc33e94c64841da89bb59d03eaf93188cede
Igor Batista Paiva <igorbatistapaiva@outlook.com>
2019-07-09 20:09:03 -0300

    Create test to tables user_associated? method

--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 RSpec.describe Table, :type => :model do
   describe '#associated_players' do
-    let!(:table) { create(:table) }
+    let!(:table) { create(:table, max_players: 5) }
 
-    let(:other_table) { create(:table) }
+    let(:other_table) { create(:table, max_players: 5) }
 
     let!(:other_player_tables) do
       create_list(:player_table, 3, table: other_table)
@@ -86,9 +86,9 @@ RSpec.describe Table, :type => :model do
   end
 
   describe '#count_players' do
-    let!(:table) { create(:table) }
+    let!(:table) { create(:table, max_players: 10) }
 
-    let(:other_table) { create(:table) }
+    let(:other_table) { create(:table, max_players: 5) }
 
     let!(:other_player_tables) do
       create_list(:player_table, 3, table: other_table)
@@ -109,6 +109,60 @@ RSpec.describe Table, :type => :model do
 
       it 'return the quantity of players' do
         is_expected.to eq player_tables.length
+      end
+    end
+  end
+
+  describe '#user_associated?' do
+    let!(:table) { create(:table, max_players: 10) }
+
+    let!(:player_tables) do
+      create_list(:player_table, 3, table: table)
+    end
+
+    let(:other_table) { create(:table, max_players: 5) }
+
+    let!(:other_player_tables) do
+      create_list(:player_table, 3, table: other_table)
+    end
+
+    context 'when the user is not associated' do
+      let!(:user) { create(:user) }
+
+      subject { table.user_associated?(user) }
+
+      it 'return false' do
+        is_expected.to be false
+      end
+    end
+
+    context 'when the user is associated' do
+      let!(:user) { create(:user) }
+
+      subject { table.user_associated?(user) }
+
+      context 'and it is a player' do
+        let(:player) { create(:player, user: user) }
+
+        let!(:player_table) do
+          create(:player_table, table: table, player: player)
+        end
+
+        it 'return true' do
+          is_expected.to be true
+        end
+      end
+
+      context 'and it is a master' do
+        let(:master) { create(:master, user: user) }
+
+        let!(:master_table) do
+          create(:master_table, table: table, master: master)
+        end
+
+        it 'return true' do
+          is_expected.to be true
+        end
       end
     end
   end
