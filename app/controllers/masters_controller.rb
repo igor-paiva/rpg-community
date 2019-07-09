7488034e014daf4b40e6b9f0838ac741abeee670
Rhuan Carlos <rhuancarlos.queiroz@gmail.com>
2019-07-09 18:18:20 -0300

    Added change role feature

--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -2,4 +2,14 @@ class MastersController < ApplicationController
   def index
     @masters = Master.availables
   end
+
+  def enable
+    @master = Master.find_or_create_by(user_id: params[:admin_id])
+  end
+
+  def update
+    @master = Master.find(params[:id])
+    @master.update_attributes params.require(:master)
+                                    .permit(:available, :campaigns)
+  end
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,14 @@ class PlayersController < ApplicationController
   def index
     @players = Player.availables
   end
+
+  def enable
+    @player = Player.find_or_create_by(user_id: params[:user_id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+    @player.update_attributes params.require(:player)
+                                    .permit(:available, :campaigns)
+  end
 end
